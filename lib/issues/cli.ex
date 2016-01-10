f5b7727223883bd9e6fe0be7ef1bd07062f7a9aa
Jamie White <jamie@jgwhite.co.uk>
2016-01-10 22:47:03 +0000

    Add Issues.TableFormatter

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,5 +1,6 @@
 defmodule Issues.CLI do
   import String, only: [to_integer: 1]
+  import Issues.TableFormatter, only: [print_table_for_columns: 2]
 
   @default_count 4
 
@@ -47,6 +48,7 @@ defmodule Issues.CLI do
     |> decode_response
     |> sort_into_ascending_order
     |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
